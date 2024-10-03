62c625976fe1b13bd7e2d43dee79cc46d8203a2d
charlieweinberger <charlieweinberger05@gmail.com>
2024-10-03 15:01:33 -0700

    moved everything from other branches & css files into this branch & tailwind

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,16 @@ export default function Footer() {
   return (
     <div>
 
-      {/* see youtube playlist: https://www.youtube.com/playlist?list=PLIq-E89khPZ-0UyoJ11BUcyj8-z41zFp5 */}
-      {/* top is wavy/layered, but then below that is contact information (plus a CTA?) */}
-      {/* call to action: large button for resume/email/github/etc., with 4 curved arrows in each corner pointing to it */}
+      Footer
+
+      {/*
+      
+        - see youtube playlist: https://www.youtube.com/playlist?list=PLIq-E89khPZ-0UyoJ11BUcyj8-z41zFp5
+        - top is wavy/layered, but then below that is contact information + CTA (aka same as the header, but flipped 180 degrees)
+        - not sticky (as opposed to the header, which is sticky)
+        - all to action: large button for resume/email/github/etc., with 4 curved arrows in each corner pointing to it
+      
+      */}
 
     </div>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,29 @@
 export default function Header() {
   return (
-    <div className="header">
+    <div>
       
       {/* See youtube playlist: https://www.youtube.com/playlist?list=PLIq-E89khPZ-0UyoJ11BUcyj8-z41zFp5 */}
 
-      <ul>
-        <li className=""><a href="#about">Charlie Weinberger</a></li>
-        <li className="float-right"><a href="#about">About</a></li>
-        <li className="float-right"><a href="#experience">Experience</a></li>
-        <li className="float-right"><a href="#projects">Projects</a></li>
+      <ul className="bg-neutral-700 fixed w-full">
+
+        <li className="float-left block text-white text-center p-4 no-underline hover:bg-neutral-900">
+          <a href="#about">Charlie Weinberger</a>
+        </li>
+
+        {/* The items below are in reverse-order */}
+        
+        <li className="float-right block text-white text-center p-4 no-underline hover:bg-neutral-900">
+          <a href="#projects">Projects</a>
+        </li>
+        
+        <li className="float-right block text-white text-center p-4 no-underline hover:bg-neutral-900">
+          <a href="#experience">Experience</a>
+        </li>
+        
+        <li className="float-right block text-white text-center p-4 no-underline hover:bg-neutral-900">
+          <a href="#about">About</a>
+        </li>
+
       </ul>
 
     </div>
