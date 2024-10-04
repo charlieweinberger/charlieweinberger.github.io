a88dea14ebef46a944d1cabb8d925e868b84d3f5
charlieweinberger <charlieweinberger05@gmail.com>
2024-10-03 22:41:59 -0700

    Finished spacing for about section

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -4,34 +4,20 @@ import Image from 'next/image';
 
 export default function About() {
   return (
-    <div id="about" className="flex flex-col justify-center items-center h-screen bg-pink-400">
-      <div className="flex w-2/3 h-2/3 blackBorder">
+    <div id="about" className="flex justify-center items-center h-screen gap-[84px] blackBorder bg-pink-400">
         
         {/* Name, Role Location, University, Links */}
-        <div className="flex items-start flex-col px-10 py-4 w-1/2 text-2xl blackBorder">
+        <div className="flex items-start flex-col px-10 py-4 w-4/12 text-2xl blackBorder">
           <div className="text-8xl leading-[1.1] my-8 text-left blackBorder">Charlie Weinberger</div>
           {middleInformation()}
           {links()}
         </div>
 
         {/* Picture (must be square) */}
-        <div className="flex justify-center items-center w-1/2">
-          <Image 
-            src="/christian.png"
-            width={0}
-            height={0}
-            sizes="1"
-            alt="Picture of Christian"
-            style={{
-              height: "80%",
-              width: "auto",
-              borderRadius: "50%",
-              objectFit: "cover",
-            }}
-          />
+        <div className="flex justify-center items-center w-1/3">
+          {picture()}
         </div>
 
-      </div>
     </div>
   );
 }
@@ -40,36 +26,11 @@ export default function About() {
 
 function middleInformation() {
   return (
-
     <div className="grid grid-cols-[30px_minmax(110px,_1fr)] justify-start text-left gap-4 p-4 blackBorder">
-
       {middleInfoLine(faLaptop, "Fullstack Web Developer")}
       {middleInfoLine(faLocationDot, "Pasadena, California / Irvine, California")}
       {middleInfoLine(faSchool, "University of California, Irvine")}
-
-      {/* <div className="flex justify-center items-center">
-        <FontAwesomeIcon icon={faLaptop} className="h-5" />
-      </div>
-      <div className="text-2xl">
-        Fullstack Web Developer
-      </div>
-
-      <div className="flex justify-center items-center">
-        <FontAwesomeIcon icon={faLocationDot} className="h-5" />
-      </div>
-      <div className="text-2xl">
-        Pasadena, California / Irvine, California
-      </div>
-
-      <div className="flex justify-center items-center">
-        <FontAwesomeIcon icon={faSchool} className="h-5" />
-      </div>
-      <div className="text-2xl">
-        University of California, Irvine
-      </div> */}
-
     </div>
-
   );
 }
 
@@ -90,7 +51,7 @@ function middleInfoLine(icon: IconDefinition, text: string) {
 
 function links() {
   return (
-    <div className="flex justify-center items-center my-10 pr-4 gap-4 w-full blackBorder"> {/* replace with shadcn/ui buttons? */}
+    <div className="flex justify-center items-center my-10 pr-4 gap-4 w-full">
       {professionalLink("Resume")}
       {professionalLink("LinkedIn")}
       {professionalLink("GitHub")}
@@ -100,8 +61,28 @@ function links() {
 
 function professionalLink(text: string) {
   return (
-    <button className="blackBorder p-4 w-full">
+    <button className="p-4 w-full border"> {/* replace with shadcn/ui buttons? */}
       {text}
     </button>
+  );
+}
+
+// Picture
+
+function picture() {
+  return (
+    <Image 
+      src="/christian.png"
+      width={0}
+      height={0}
+      sizes="1"
+      alt="Picture of Christian"
+      style={{
+        height: "auto",
+        width: "auto",
+        borderRadius: "50%",
+        objectFit: "cover",
+      }}
+    />
   );
 }
