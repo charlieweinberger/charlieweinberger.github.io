1df9fd1a99c0abec8ce1469e4f20166e1f02cd6f
charlieweinberger <charlieweinberger05@gmail.com>
2024-10-06 23:40:28 -0700

    more shit that doesnt work

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -4,32 +4,43 @@ import Image from 'next/image';
 
 export default function About() {
   return (
-    <div id="about" className="flex justify-center items-center h-screen gap-[128px] bg-neutral-950">
+    <div id="about" className="h-screen flex flex-wrap justify-center items-center p-16 gap-48 bg-neutral-950">
         
         {/* Name, Role Location, University, Links */}
-        <div className="flex items-start flex-col py-4 w-[32.15%] text-2xl">
-          <div className="my-8 text-8xl text-left font-bold leading-[1.1]">Charlie Weinberger</div>
-          {middleInformation()}
-          {links()}
+        <div className="w-[30%] min-w-[460px] flex items-start flex-col gap-14 text-2xl">
+          
+          <div className="AboutSectionName text-left font-bold leading-[1.1]">
+            Charlie Weinberger
+          </div>
+          
+          <div className="grid grid-cols-[30px_minmax(110px,_1fr)] justify-start text-left gap-4">
+            {middleInfoLine(faLaptop, "Fullstack Web Developer")}
+            {middleInfoLine(faLocationDot, "Pasadena, California / Irvine, California")}
+            {middleInfoLine(faSchool, "University of California, Irvine")}
+          </div>
+          
+          <div className="w-full flex justify-center items-center gap-6">
+            {profileLink("Resume", "/Charlie_Weinberger_Resume.pdf")}
+            {profileLink("LinkedIn", "https://linkedin.com/in/charlie-weinberger/")}
+            {profileLink("GitHub", "https://github.com/charlieweinberger/")}
+          </div>
+
         </div>
 
         {/* Picture (must be square) */}
-        <div className="flex justify-center items-center w-1/3">
-          {picture()}
+        <div className="flex justify-center items-center w-[33%]">
+          <Image 
+            src="/bonfire.png"
+            width={768}
+            height={768}
+            alt="Picture of Charlie Weinberger"
+            style={{
+              borderRadius: "50%",
+              objectFit: "cover",
+            }}
+          />
         </div>
 
-    </div>
-  );
-}
-
-// MiddleInformation
-
-function middleInformation() {
-  return (
-    <div className="grid grid-cols-[30px_minmax(110px,_1fr)] justify-start text-left gap-4 py-4">
-      {middleInfoLine(faLaptop, "Fullstack Web Developer")}
-      {middleInfoLine(faLocationDot, "Pasadena, California / Irvine, California")}
-      {middleInfoLine(faSchool, "University of California, Irvine")}
     </div>
   );
 }
@@ -40,53 +51,21 @@ function middleInfoLine(icon: IconDefinition, text: string) {
       <div className="flex justify-center items-center">
         <FontAwesomeIcon icon={icon} className="h-5" />
       </div>
-      <div className="text-2xl">
+      <p>
         {text}
-      </div>
+      </p>
     </>
-  );
-}
-
-// Links
-
-function links() {
-  return (
-    <div className="flex justify-center items-center my-10 pr-1 gap-6 w-full">
-      {profileLink("Resume", "/Charlie_Weinberger_Resume.pdf")}
-      {profileLink("LinkedIn", "https://linkedin.com/in/charlie-weinberger/")}
-      {profileLink("GitHub", "https://github.com/charlieweinberger/")}
-    </div>
   );
 }
 
 function profileLink(text: string, link: string) {
   return (
     <a
-      className="p-4 w-full border rounded-xl hover:bg-neutral-50 hover:text-neutral-950 active:bg-gray-300 active:text-neutral-950"
+      className="w-full py-4 px-6 border rounded-xl hover:bg-neutral-50 hover:text-neutral-950 active:bg-gray-300 active:text-neutral-950"
       href={link}
       target="_blank"
     >
       {text}
     </a>
-  );
-}
-
-// Picture
-
-function picture() {
-  return (
-    <Image 
-      src="/bonfire.png"
-      width={0}
-      height={0}
-      sizes="1"
-      alt="Picture of Charlie Weinberger"
-      style={{
-        height: "500px",
-        width: "auto",
-        borderRadius: "50%",
-        objectFit: "cover",
-      }}
-    />
   );
 }
